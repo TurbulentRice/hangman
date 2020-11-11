f72271de40fc4fdb43102e0dda6d71935235b1ab
Sean Russell <seanrussellphotography@gmail.com>
2020-11-11 17:51:10 -0500

    Fixed bug in while loop for entering guess.

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # hangman
 
-Javascript hangman guessing game.
+Albany Can Code: Javascript Fundamentals, Fall '20 cohort midterm assignment. It's a Javascript hangman guessing game!
 
 ### Rules:
 
 1. Enter "y" or "yes" at the start to play. To stop, enter "n" or "no" when asked to play, or hit ctrl+c to exit at any time.
-2. Select a difficulty (4-6), which will determine the length of the random mystery word that round.
-3. Player wins when word all letters are guessed correctly.
+2. Select a difficulty (4-6), which will determine the length of the random mystery word for that round.
+3. Player wins when all letters are guessed correctly.
 4. Player loses after 6 "strikes." Only incorrect guesses count as strikes. Repeat guesses will prompt the user for a different guess.
-5. Rounds played and rounds won are displayed at the start of the game.
+5. Rounds played and rounds won are displayed at the start of a new game.
 
 ![Hangman game screenshot](hangman_screen.jpg)
 
 ### Coding features:
 
-- Three selectable difficulties
+- Selectable difficulties
 - Hanged-man visual counter
 - Higher order functions (map() and reduce())
+- Closures
 - Currying
 - RegEx
 - Arrow functions
--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const STRIKES = 6
 // Game object
 const hangmanGame = (difficulty) => {
   // Functions
-
   // Format and display a string array 
   const show = (arr, sep=" ") => console.log(`\n${arr.join(sep)}\n`)
-
   // Get a random word from wordBank (takes an optional length arg equal to difficulty)
   const randomWord = (length=0) => {
     const getRand = () => wordBank[Math.floor(Math.random() * wordBank.length)].toLowerCase().split('');
@@ -21,23 +19,21 @@ const hangmanGame = (difficulty) => {
     }
     return getRand();
   };
-
-  // Display the hangman based on how many guesses left
-  const hangMan = () => {
+  // Display the hanged man based on how many guesses left
+  const hangedMan = () => {
     const man = [' O', '\n\\|', '/', '\n |', '\n/', ' \\']
     guessesLeft < STRIKES && show(man.slice(0, (STRIKES - guessesLeft)), sep="")
   }
-  
-  // Counters
+
+  // Variables
   let guessesLeft = STRIKES
   let guessedLetters = [];
-
-  // Words
   const word = randomWord(difficulty);
   let hiddenWord = word.map(() => "_")
+  
   show(hiddenWord)
 
-  // Guessing Function
+  // Guessing closure is returned
   return (guess) => {
     // Handle already guessed
     if (guessedLetters.includes(guess)) {
@@ -45,33 +41,33 @@ const hangmanGame = (difficulty) => {
       return guessesLeft;
     } else guessedLetters.push(guess)
 
-    // Check guess (reduce word to list of indices where matches occur)
+    // Check guess against word (reduce word to list of indices where matches occur)
     let matches = word.reduce((correct, letter, idx) => {
       return letter === guess ? [...correct, idx] : correct;
     }, []);
 
-    // If matches is empty, count as strike, otherwise reveal matches in hiddenWord
+    // If matches is empty, count as strike, otherwise "reveal" the letter(s) in hiddenWord
     !matches.length ? guessesLeft-- : matches.forEach(idx => hiddenWord[idx] = guess)
 
     // Show progress (hangman and word)
-    hangMan() === undefined && show(hiddenWord)
+    // handedMan() will always === undefined, this is shortcut to execute both functions
+    hangedMan() === undefined && show(hiddenWord)
 
     // No guesses left: Show word
     !guessesLeft && console.log(`You're out of guesses! The word was:\n\n${word.join(" ")}\n`)
 
-    // If hiddenWord is incomplete, return guessesLeft, otherwise you win
+    // If hiddenWord is incomplete, return guessesLeft to game loop, otherwise you win
     return hiddenWord.includes("_") ? guessesLeft : "win"; 
   }
 }
 
-// Game loop
+// Game loop IIFE
 (function () {
-  // Game counter
+  // Counters
   let gamesStarted = 0;
   let gamesWon = 0;
 
-  // Main loop
-  while (prompt.question("\nPlay a game? y/n: ").toLowerCase()[0] === "y") {
+  while (prompt.question("\nWant to play a round of hangman? y/n: ").toLowerCase()[0] === "y") {
     // Intro
     gamesStarted++;
     console.log(`
@@ -84,15 +80,19 @@ Round ${gamesStarted}!`)
     gamesStarted > 1 && console.log(`Games won: ${gamesWon} / ${gamesStarted-1}`);
     console.log('-------------------')
 
-    // Ask user for difficulty: 4-6 (looped to reject nonNumbers)
-    let difficulty = 0;
+    // Ask user for difficulty: 4-6
+    let difficulty;
     while (!difficulty || difficulty < 4 || difficulty > 6) {
-      difficulty = Number(prompt.question("\nWhat difficulty? (4-6): "))
+      difficulty = Number(prompt.question(`
+Select a difficulty (4-6)
+(4) Easy
+(5) Medium
+(6) Hard
+Enter a number: `))
     }
 
     // Start guessing game
     const myGame = hangmanGame(difficulty)
-    // Initialize result var, after this it will be = returned value from game
     let result = STRIKES;
     while (result) {
       // Handle win
@@ -100,22 +100,20 @@ Round ${gamesStarted}!`)
         gamesWon++;
         break;
       }
-
-      // Display strikes left 
+ 
       console.log(`\nYou have ${result} strikes left.\n`)
 
-      // Get a guess, loop until a letter is provided
-      let thisGuess="";
-      while (!(/[a-zA-Z]/).test(thisGuess)) {
-        thisGuess = prompt.question("Enter a letter: ")[0].toLowerCase()
+      // Get a guess
+      let thisGuess ="";
+      while (!(/[a-zA-Z]/).test(thisGuess) || !thisGuess) {
+        thisGuess = prompt.question("Enter a letter: ").toLowerCase()[0]
       }
       console.log(`\nYou guessed "${thisGuess}" ...`)
       
-      // Submit guess, receive strikesLeft (when 0, exit main loop)
+      // Submit guess, store strikesLeft in result
       result = myGame(thisGuess);
     };
     console.log(result ? "You win!" : "Better luck next time!");
   };
-  // Exit main loop
-  console.log("Goodbye!")
+  console.log("\nGoodbye!\n")
 })();
